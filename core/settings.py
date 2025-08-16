fad5b431b543a1bb8283aa56bda5a4f2ea3b6290
carl-dev <carltonmbaja0@gmail.com>
2025-08-16 18:14:41 +0300

    fix(deploy): align template path with static root for production

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'client/dist')],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
